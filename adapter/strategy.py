dc838f88529edcb34c583466002e96c629044b76
ronnifadhlillah <33772449+ronnifadhlillah@users.noreply.github.com>
2021-10-22 01:05:26 +0700

    deleted port validation
    because ldap port is too native

--- adapter/strategy.py
+++ adapter/strategy.py
@@ -49,17 +49,16 @@ class Strategy:
         return Strategy.doAuthentication(self,username,base_dn,dn)
 
     def WinAD(self):
-        username='%s@%s' % (self.un,'.'.join(self.dc[0:]))
         dn=[]
         for i in self.dc[0:]:
             dn.append(str(i))
         if tldextract.extract(self.uri).subdomain is not '':
             bj=',DC='.join(dn)
-            base_dn=str('CN='+bj)
+            base_dn=str('DC='+bj)
         else:
             bj=',DC='.join(dn)
             base_dn=str('DC='+bj)
-        return Strategy.doAuthentication(self,username,base_dn,dn)
+        return Strategy.doAuthentication(self,base_dn,dn)
 
     def DomainValidate(self):
             # Validation URI parameter
@@ -71,21 +70,17 @@ class Strategy:
             pattern=re.match('((http|https)://)(www.)?[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)',self.uri)
             if bool(pattern) == True:
                 return False
-            # Port validation
-            port=[995,993,465,143,389,50000,50001]
-            if self.prt not in port:
-                return False
 
-    def doAuthentication(self,username,base_dn,dn):
+    def doAuthentication(self,base_dn,dn):
         addr=socket.gethostbyname(self.uri.upper())
         l=ldap.initialize('ldap://%s' % addr)
         l.protocol_version=ldap.VERSION3
         l.set_option(ldap.OPT_REFERRALS,self.prt)
         try:
-            l.simple_bind_s(username,self.ps)
-            # LDAP testing below is currently running on linux (smb4DAD only)
-            # attr=['memberOf'] #--> For testing the AD
-            # # testing ldap connection --> For testing the AD
+            l.simple_bind_s(self.un,self.ps)
+            # # LDAP testing below is currently running on linux (smb4DAD only)
+            # attr=['Domain'] #--> For testing the AD
+            # # # testing ldap connection --> For testing the AD
             # auth=l.search_s(base_dn,ldap.SCOPE_SUBTREE,'(objectClass=*)',attr) #--> For testing the AD
             # for dn,entry in auth: #--> For testing the AD
             #     print('Processing',repr(entry)) #--> For testing the AD
